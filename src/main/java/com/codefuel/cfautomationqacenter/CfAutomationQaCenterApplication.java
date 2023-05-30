effd62e6b2df65cc5561619f10f0daf2ce4c8b36
anastasiya <anastasiyas@codefuel.com>
2023-05-30 14:22:46 +0300

    Initial commit

---
+++ src/main/java/com/codefuel/cfautomationqacenter/CfAutomationQaCenterApplication.java
@@ -0,0 +1,13 @@
+package com.codefuel.cfautomationqacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfAutomationQaCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfAutomationQaCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codefuel/cfautomationqacenter/CfAutomationQaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefuel.cfautomationqacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfAutomationQaCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
